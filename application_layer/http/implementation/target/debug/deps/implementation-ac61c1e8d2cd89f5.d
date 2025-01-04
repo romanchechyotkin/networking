210d36254be6375d4dad6d3393833832b3ca86d2
Chechetkin Roman Aleksandrovich <rchechetkin@ozon.ru>
2025-01-04 20:56:58 +0300

    init rust implementation

--- application_layer/http/implementation/target/debug/deps/implementation-ac61c1e8d2cd89f5.d
+++
@@ -1,5 +0,0 @@
-/Users/rchechetkin/networking/application_layer/http/implementation/target/debug/deps/libimplementation-ac61c1e8d2cd89f5.rmeta: src/main.rs
-
-/Users/rchechetkin/networking/application_layer/http/implementation/target/debug/deps/implementation-ac61c1e8d2cd89f5.d: src/main.rs
-
-src/main.rs:
--- application_layer/http/implementation/target/debug/implementation.d
+++
@@ -1,1 +0,0 @@
-/Users/rchechetkin/networking/application_layer/http/implementation/target/debug/implementation: /Users/rchechetkin/networking/application_layer/http/implementation/src/main.rs
